30e9fce38b81305c2bf560959935afb7a6faa090
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2022-05-07 11:18:59 +0100

    cuur stater

--- index.js
+++ index.js
@@ -2,23 +2,47 @@ function show(id) {
   let aaheight = window.innerHeight
   document.getElementById(id).style.display = 'block'
   aaheight -= document.querySelector('.title').offsetHeight
-  document.getElementById(id).style.height = `${aaheight}px`
+  alert(aaheight)
+  document.getElementById(id).style.maxheight = `${aaheight}px`
+  document.getElementById('SEL').style.display = `none`
 }
 function currency() {
   alert('currency')
 }
 function press(button) {
-  if (operators.indexOf(button) == -1) {
+  if (button === '(-)') {
+    add('-')
+  } else if (operators.indexOf(button) == -1) {
+    add(button)
+  } else if (operators.indexOf(button) !== -1) {
     add(button)
   }
-  if (operators.indexOf(button) !== -1) {
-    add(` ${button} `)
-  }
-}
-function add(n) {
-  integer += String(n)
-  document.getElementById('output').innerHTML = integer
 }
 
+function add(n) {
+  integer += String(n)
+  document.getElementById('output').innerHTML = rep()
+}
+function final(text) {
+  if (integer != '') {
+    integer = '0'
+  }
+  ans = Obj.exec(text)
+  if (ans != 'ERROR') {
+    document.getElementById('output').innerHTML = ans
+  } else {
+    document.getElementById('output').innerHTML = "t'was an error ( ‘ o ‘ )"
+  }
+  integer = ''
+}
+function back() {
+  integer = integer.slice(0, integer.length - 2)
+  document.getElementById('output').innerHTML = integer
+}
+function checkcu() {
+  alert('x')
+  return true
+}
+let x = '/a/g'
 let integer = ''
 let operators = ['*', '/', '+', '-']
