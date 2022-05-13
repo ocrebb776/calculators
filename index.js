7f780f3cb829ba4a0e7b75ef43fc8b01fcd734d0
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2022-05-13 19:50:17 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function show(id) {
   let aaheight = window.innerHeight
   document.getElementById(id).style.display = 'block'
-  aaheight -= document.querySelector('.title').offsetHeight
-  alert(aaheight)
+  aaheight -= document.querySelector('.title').Height
   document.getElementById(id).style.maxheight = `${aaheight}px`
   document.getElementById('SEL').style.display = `none`
 }
@@ -10,6 +9,10 @@ function currency() {
   alert('currency')
 }
 function press(button) {
+  if (ans != '') {
+    integer += ans
+    ans = ''
+  }
   if (button === '(-)') {
     add('-')
   } else if (operators.indexOf(button) == -1) {
@@ -18,22 +21,29 @@ function press(button) {
     add(button)
   }
 }
-
+let rate = 0
+let start = 0
+let time = 0
+let ans = ''
 function add(n) {
   integer += String(n)
-  document.getElementById('output').innerHTML = rep()
+  document.getElementById('output').innerHTML = integer
 }
 function final(text) {
   if (integer != '') {
     integer = '0'
   }
+  console.log(text)
+  text = rep(text)
   ans = Obj.exec(text)
+  console.log(text)
   if (ans != 'ERROR') {
     document.getElementById('output').innerHTML = ans
   } else {
     document.getElementById('output').innerHTML = "t'was an error ( ‘ o ‘ )"
   }
   integer = ''
+  ans = String(ans)
 }
 function back() {
   integer = integer.slice(0, integer.length - 2)
@@ -44,8 +54,33 @@ function checkcu() {
 }
 let x = '/a/g'
 let integer = ''
-let operators = ['*', '/', '+', '-']
-const box = document.getElementById('iframex')
+let operators = [' + ', ' ÷ ', ' X ', ' - ']
+let finala = 0
+let ter = ''
+go = function (forw) {
+  if (forw) {
+    document.getElementById('fx').style.display = 'none'
+    document.getElementById('h1').style.display = 'none'
+    document.getElementById('calc').style.display = 'none'
+    document.getElementById('rest').style.display = 'block'
+    var form = document.getElementById('fx')
+    rate = form.elements['rate'].value
+    start = form.elements['value'].value
+    time = form.elements['time'].value
+    for (let qwe = 0; qwe <= time; qwe++) {
+      finala = start * (1 + rate / 100) ** qwe
+      ter += `<tr><td>${String(Math.round(qwe))}</td><td>${String(
+        Math.round((finala + Number.EPSILON) * 100) / 100,
+      )}</td></tr>`
+    }
+    var asd = '<tr><th><b>Time</th><th>Balence(£)</th></tr></b>'
+    ter = asd + ter
 
-// 👇️ Apple, Banana, Kiwi
-console.log(box.textContent)
+    document.getElementById('insert-qw').innerHTML = ter
+  } else {
+    document.getElementById('fx').style.display = 'grid'
+    document.getElementById('h1').style.display = 'block'
+    document.getElementById('calc').style.display = 'block'
+    document.getElementById('rest').style.display = 'none'
+  }
+}
